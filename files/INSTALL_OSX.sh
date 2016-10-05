b990ce3a5a41739d273b910464793cd71cbe9c19
Jordan <Dill@Leahs-MacBook-Air.local>
2016-10-05 09:05:05 -0400

    updates

--- files/INSTALL_OSX.sh
+++ files/INSTALL_OSX.sh
@@ -20,9 +20,9 @@ pip install --upgrade setuptools
 # Python libraries
 sudo -H pip install beautifulsoup4 Bottleneck colorama cython joblib matplotlib numexpr numpy opencv-python pandas psutil PySAL PyYAML retrying Rtree scikit-image scikit-learn scipy six tables xmltodict
 
-#brew install gdal2 --with-hdf4 --with-hdf5
-#echo /usr/local/opt/gdal2/lib/python2.7/site-packages >> /usr/local/lib/python2.7/site-packages/gdal2.pth
-#brew link --force gdal2
+brew install gdal2 --with-hdf4 --with-hdf5
+echo /usr/local/opt/gdal2/lib/python2.7/site-packages >> /usr/local/lib/python2.7/site-packages/gdal2.pth
+brew link --force gdal2
 
 # SpFeas
 pip install SpFeas-0.0.1.tar.gz
