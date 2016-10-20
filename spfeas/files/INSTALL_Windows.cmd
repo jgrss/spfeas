c03e336d68202e1345d3d3a0581ff29f64f20212
Jordan <Brome@comm685-0303-dhcp-033.bu.edu>
2016-10-20 10:07:14 -0400

    modified:   spfeas/files/INSTALL_Windows.cmd 	modified:   spfeas/notebooks/INSTALLATION.ipynb

--- spfeas/files/INSTALL_Windows.cmd
+++ spfeas/files/INSTALL_Windows.cmd
@@ -1,10 +1,18 @@
 IF where pip > NUL (
+
     pip install beautifulsoup4 Bottleneck colorama cython joblib matplotlib numexpr numpy opencv-python pandas PySAL retrying scikit-image scikit-learn scipy six tables xmltodict 
    
-    pip uninstall mpglue 
-    pip install MpGlue-0.0.1.tar.gz
-    pip uninstall spfeas
-    pip install SpFeas-0.0.1.tar.gz
+    IF where classify > NUL (
+        pip uninstall mpglue
+      )
+
+    pip install git+https://github.com/jgrss/mpglue.git
+
+    IF where spfeas > NUL (
+        pip uninstall spfeas
+      ) 
+
+    pip install git+https://github.com/jgrss/spfeas.git     
 
     IF where spfeas > NUL (
         ECHO.The installation has finished!
