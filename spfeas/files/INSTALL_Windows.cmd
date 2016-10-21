6da7cb5dd4a392dd64dd5dc9326d6d128fe93895
Jordan <Brome@comm685-0303-dhcp-033.bu.edu>
2016-10-21 16:57:57 -0400

    modified:   spfeas/files/INSTALL_Windows.cmd 	modified:   spfeas/notebooks/INSTALLATION.ipynb

--- spfeas/files/INSTALL_Windows.cmd
+++ spfeas/files/INSTALL_Windows.cmd
@@ -5,7 +5,7 @@ IF NOT ERRORLEVEL 1 (
 
     pip install beautifulsoup4 retrying six tables xmltodict
 
-    pip install numpy-1.11.2+mkl-cp27-cp27m-win32.whl colorama-0.3.7-py2.py3-none-any.whl Cython-0.24.1-cp27-cp27m-win32.whl joblib-0.10.2-py2.py3-none-any.whl matplotlib-2.0.0b4-cp27-cp27m-win32.whl numexpr-2.6.1-cp27-cp27m-win32.whl opencv_python-3.1.0-cp27-cp27m-win32.whl pandas-0.19.0-cp27-cp27m-win32.whl PySAL-1.12.0-py2-none-any.whl scikit_image-0.12.3-cp27-cp27m-win32.whl scikit_learn-0.18-cp27-cp27m-win32.whl scipy-0.18.1-cp27-cp27m-win32.whl 
+    pip install numpy-1.11.2+mkl-cp27-cp27m-win32.whl numexpr-2.6.1-cp27-cp27m-win32.whl scikit_image-0.12.3-cp27-cp27m-win32.whl scipy-0.18.1-cp27-cp27m-win32.whl tables-3.3.0-cp27-cp27m-win32.whl 
    
     REM Uninstall MpGlue if it already exists 
     where /q classify
