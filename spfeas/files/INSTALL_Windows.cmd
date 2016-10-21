00a9eb82adeb0fd100bc3f457cb54acdcc4f807d
Jordan <Brome@comm685-0303-dhcp-033.bu.edu>
2016-10-21 15:48:44 -0400

    modified:   spfeas/files/INSTALL_Windows.cmd

--- spfeas/files/INSTALL_Windows.cmd
+++ spfeas/files/INSTALL_Windows.cmd
@@ -1,20 +1,25 @@
-IF where pip > NUL (
+where /q pip
+
+IF NOT ERRORLEVEL 1 (
 
     pip install beautifulsoup4 Bottleneck colorama cython joblib matplotlib numexpr numpy opencv-python pandas PySAL retrying scikit-image scikit-learn scipy six tables xmltodict 
-   
-    IF where classify > NUL (
+    
+    where /q classify
+    IF NOT ERRORLEVEL 1 (
         pip uninstall mpglue
       )
 
     pip install git+https://github.com/jgrss/mpglue.git
 
-    IF where spfeas > NUL (
+    where /q spfeas
+    IF NOT ERRORLEVEL 1 (
         pip uninstall spfeas
       ) 
 
     pip install git+https://github.com/jgrss/spfeas.git     
 
-    IF where spfeas > NUL (
+    where /q spfeas
+    IF NOT ERRORLEVEL 1 (
         ECHO.The installation has finished!
       ) ELSE (
         ECHO.SpFeas failed to install.
