7a35f7c8a03060cdf0450274415b5e9c753450ee
jgrss <graesser@bu.edu>
2016-11-23 17:49:49 -0500

    updates

--- spfeas/files/INSTALL_OSX.sh
+++ spfeas/files/INSTALL_OSX.sh
@@ -18,11 +18,14 @@ else
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update the .profile
+# Create the .profile
 if [ ! -f ~/.profile ]; then
   touch ~/.profile
 fi
 
+#export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+
+# Add the NumPy core directory to .profile
 if [ -z ${CFLAGS} ]; then 
   echo 'export CFLAGS=-I/usr/local/lib/python2.7/site-packages/numpy/core/include/' >>~/.profile
   source ~/.profile
@@ -30,24 +33,31 @@ else
   echo "CFLAGS is already set to '$CFLAGS'"
 fi
 
+# gcc 6
+if [ -z ${CC} ]; then
+  echo 'export CC=gcc-6' >>~/.profile
+  source ~/.profile
+else
+  echo "CC is already set to '$CC'"
+fi
+
 # Homebrew
 brew tap osgeo/osgeo4mac
 brew tap homebrew/science
 brew tap homebrew/versions
-#brew install wget spatialindex
-#brew install gcc python hdf4 hdf5 spatialindex
+#brew install wget
+brew install gcc hdf4 hdf5 spatialindex
 
 # Python from Homebrew
-if which python >/dev/null; then
-  echo 'Python is already installed'
-else
-  brew install python
-  brew linkapps python
-fi
+#if which python >/dev/null; then
+#  echo 'Python is already installed'
+brew install python
+brew linkapps python
 
 # Python pip
 if which pip >/dev/null; then
   echo 'pip is already installed'
+  sudo -H pip install --upgrade pip
 else
   sudo -H easy_install pip
 fi
@@ -66,11 +76,18 @@ echo
 # Python libraries
 sudo -H pip install beautifulsoup4 Bottleneck colorama cython joblib matplotlib numexpr numpy opencv-python pandas psutil PySAL PyYAML retrying Rtree scikit-image scikit-learn scipy six tables xmltodict
 
+# Statsmodels (0.8 is not available from pip)
+git clone https://github.com/statsmodels/statsmodels.git
+cd statsmodels/
+python setup.py build
+python setup.py install
+cd ..
+rm -rf statsmodels/
+
 # GDAL from Homebrew
 if which gdalinfo >/dev/null; then
   echo 'GDAL is already installed'
 else
-  brew install hdf4 hdf5 spatialindex
   brew install gdal2 --with-hdf4 --with-hdf5
   echo /usr/local/opt/gdal2/lib/python2.7/site-packages >> /usr/local/lib/python2.7/site-packages/gdal2.pth
   brew link --force gdal2
