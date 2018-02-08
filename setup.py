b1b88f8bdc9419314159fa1df98f150bb805e848
jgrss <jbgraesser@gmail.com>
2018-02-08 17:47:26 -0500

    updated version from 0.2.3 to 0.2.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except:
 import numpy as np
 
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 spfeas_name = 'SpFeas'
 maintainer = 'Jordan Graesser'
--- spfeas/__init__.py
+++ spfeas/__init__.py
@@ -1,3 +1,3 @@
 from spfeas import spatial_features
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
